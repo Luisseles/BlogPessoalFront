2500e3a9b3ea56a4640585918fba9d1ae2e34486
Luis Henrique <selesluishenrique@gmail.com>
2022-06-23 12:38:05 -0300

    :up: Atualizações e métodos put-delete de tema

--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -23,4 +23,12 @@ export class TemaService {
   postTema(tema: Tema): Observable<Tema> {
     return this.http.post<Tema>("http://localhost:8080/tema", tema, this.token)
   }
+
+  putTema(tema: Tema): Observable<Tema> {
+    return this.http.put<Tema>("http://localhost:8080/tema", tema, this.token)
+  }
+
+  deleteTema(id: number) {
+    return this.http.delete(`http://localhost:8080/tema/${id}`, this.token)
+  }
 }
